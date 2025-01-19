45785f58aa63e568bb2a4f70927bf37ff9f73e0b
betashort <31877309+betashort@users.noreply.github.com>
2025-01-20 00:14:21 +0900

    a

--- source/mhd-next/src/app/page.tsx
+++ source/mhd-next/src/app/page.tsx
@@ -5,97 +5,52 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          className=""
+          src="/mhd-image.png"
+          alt="mhd image"
+          width={800}
+          height={800}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div>
+          <h2 className="p-2 m-2 text-center bg-slate-700 text-white">
+            気分を記録するためのアプリです
+          </h2>
+          <p className="text-center">
+            ＊作成者が個人的に調べた内容です。
+            <br />
+            ＊開発中です。
+            <br />
+          </p>
+          <h3 className="p-2 m-2 text-center bg-slate-700 text-white">機能</h3>
+          <div className="discription pl-2 flex justify-center items-center">
+            <table className="">
+              <tbody>
+                <tr>
+                  <th>機能</th>
+                  <th>説明</th>
+                </tr>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                <tr>
+                  <td>Diary</td>
+                  <td>気分を記録します。</td>
+                </tr>
+                <tr>
+                  <td>活動グラフ</td>
+                  <td>活動を数値化してグラフ表示します。</td>
+                </tr>
+                <tr>
+                  <td>気分グラフ</td>
+                  <td>気分を数値化してグラフ表示します。</td>
+                </tr>
+                <tr>
+                  <td>Tips</td>
+                  <td>メンタルヘルスについて調査したことをまとめています。</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </main >
+    </div >
   );
 }
---
+++ source/mhd-next/src/component/Header.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <>
+      <div className="w-full h-10 flex justify-between items-center bg-slate-900">
+        <h1 className="text-2xl text-white">
+        <Link href="/">MentalHealthDiary App</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li className="mr-4 text-xl text-white">
+              <Link href="/tips">Tips</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
