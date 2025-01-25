11a5cfd0fbbbf36277411be509bf479ef85cada5
betashort <wshota.betashort@gmail.com>
2025-01-25 20:48:17 +0900

    error debug

--- source/mhd-next/src/app/(pages)/diary/page.tsx
+++ source/mhd-next/src/app/(pages)/diary/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { useCallback, createContext, useState } from "react";
-
 import FullCalendar from "@fullcalendar/react";
-import { EventClickArg } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
-import { Box, Modal } from "@mui/material";
 
 export default function Diary() {
   return (
