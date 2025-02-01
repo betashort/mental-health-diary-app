cc504f34ffe30ba01cd5c4928346494618fb1d32
betashort <wshota.betashort@gmail.com>
2025-02-02 00:04:26 +0900

    auth

---
+++ source/mhd-next/src/auth.ts
@@ -0,0 +1,10 @@
+
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const { auth, handlers, signIn, signOut } = NextAuth( {
+    providers:[
+        GithubProvider, GoogleProvider
+    ]
+});
