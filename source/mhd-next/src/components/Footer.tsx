b45b0ae9ed9beeb2c7e0493fbb2d70a08895cfc9
betashort <wshota.betashort@gmail.com>
2025-02-04 22:49:25 +0900

    auth footer display

--- source/mhd-next/src/components/Footer.tsx
+++ source/mhd-next/src/components/Footer.tsx
@@ -1,27 +1,40 @@
+import { auth } from '@/auth';
 import Link from 'next/link'
 
-export default function Footer() {
-  return (
+export default async function Footer() {
+  const session = await auth();
+
+  if (!session) {
+    return (
     <footer>
-      <div className="w-full h-16 bg-slate-900 fixed bottom-0">
-        <div className="grid grid-cols-3">
-          <div className="h-16 border-r-2 flex justify-center items-center text-white">
-            <div className="">
-              <Link href="/diary">日記</Link>
+      <p className='bg-slate-900 text-white flex justify-center items-center'>© 2025 βshort</p>
+    </footer>
+    );
+  }
+  else {
+    return (
+      <footer>
+        <div className="w-full h-16 bg-slate-900 fixed bottom-0">
+          <div className="grid grid-cols-3">
+            <div className="h-16 border-r-2 flex justify-center items-center text-white">
+              <div className="">
+                <Link href="/diary">日記</Link>
+              </div>
             </div>
-          </div>
-          <div className="h-16 flex justify-center items-center text-white">
-            <Link href="/mental-report">
-              <div className="">メンタルヘルスレポート</div>
-            </Link>
-          </div>
-          <div className="h-16 border-l-2 flex justify-center items-center text-white">
-            <div className="">
-              <Link href="/sns">SNS</Link>
+            <div className="h-16 flex justify-center items-center text-white">
+              <Link href="/mental-report">
+                <div className="">メンタルヘルスレポート</div>
+              </Link>
+            </div>
+            <div className="h-16 border-l-2 flex justify-center items-center text-white">
+              <div className="">
+                <Link href="/sns">SNS</Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </footer>
-  );
+      </footer>
+    );
+  }
+
 }
