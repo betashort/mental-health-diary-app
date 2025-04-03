c8db8f163861be9c8c759df39a8c0e854a1297b5
betashort <wshota.betashort@gmail.com>
2025-04-03 22:18:12 +0900

    google oauth

--- source/mhd-next/auth.config.ts
+++ source/mhd-next/auth.config.ts
@@ -1,4 +1,5 @@
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 
 export default {
@@ -7,5 +8,11 @@ export default {
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
+    Google(
+      {
+        clientId: process.env.AUTH_GOOGLE_ID,
+        clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      }
+    )
   ]
 } satisfies NextAuthConfig;
